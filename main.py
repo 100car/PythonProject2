dcd5bc251cc559fdfdd7b130396eb3c0c1846480
100car <serg@tokar.com.ua>
2025-09-02 21:28:30 +0300

    Домашнє завдання. Курс: AI+Python. Модуль 11. ООП. Тема: ООП. Частина 4.

--- main.py
+++ main.py
@@ -1,82 +1,75 @@
 # Завдання 1
-# Створіть наступні класи:
-#  CreditCardPayment – атрибути currency
-#  PayPalPayment – атрибути currency
-#  CryptoPayment – атрибути currency
-# Методи:
-#  pay(amount) – виводить повідомлення
-#   o CreditCardPayment – оплата карткою {amount}{currency}
-#   o PayPalPayment – оплата PayPal {amount}{currency}
-#   o CryptoPayment – оплата криптогаманцем {amount}{currency}
-# Напишіть функцію create_payment() яка запитує у користувача тип рахунку та потрібні атрибути і повертає
-# об’єкт. Створіть декілька рахунків, добавте їх у список та для кожної викличте відповідні методи.
+# Створіть клас Recipe з атрибутами
+#    name – назва страви
+#    ingredients – список продуктів
+#    text – текст рецепту
+#    time – час приготування
+# методи:
+#    __str__(self) – повертає назву страви
+#    __contains__(self, item) – перевіряє чи є інгредієнт в рецепті
+#    __gt__(self, other) – перевіряє чи є час приготування self більшим за other
+#    display_info(self) – виводить всю інформацію про рецепт
+# Створіть декілька рецептів та добавте їх у список.
+# Виведіть назви тих рецептів, які містять інгредієнт томат
+# Виведіть повну інформацію рецепта з найменшим часом приготування, скористайтесь функцією min
 
-class CreditCardPayment:
-    def __init__(self, currency):
-        self.currency = currency
+class Recipe:
+    def __init__(self, name, ingredients, text, time):
+        self.name = name
+        self.ingredients = ingredients or []
+        self.text = text
+        self.time = time
 
-    def pay(self, amount):
-        print(f"оплата карткою {amount} {self.currency}")
+    def __str__(self):
+        return f"{self.name}"
 
-class PayPalPayment:
-    def __init__(self, currency):
-        self.currency = currency
+    def __contains__(self, item):
+        return item.lower() in (ing.lower() for ing in self.ingredients)
 
-    def pay(self, amount):
-        print(f"оплата PayPal {amount}{self.currency}")
+    def __gt__(self, other):
+        return self.time > other.time
+        # if self.time > other:
+        #     print(f"Час {self.time} більший, ніж {other}")
+        # else:
+        #     print(f"Час {self.time} менший, ніж {other}")
 
-class CryptoPayment:
-    def __init__(self, currency):
-        self.currency = currency
+    def display_info(self):
+        print(f"Рецепт '{self.name}':")
+        print(f"\tСписок продуктів: {', '.join(self.ingredients)}.")
+        print(f"\tРецепт: {self.text}.")
+        print(f"\tЧас приготування: {self.time} хв.")
 
-    def pay(self, amount):
-        print(f"оплата криптогаманцем {amount}{self.currency}")
+recipe1 = Recipe("Піца",
+ ["борошно", "вода", "дріжджі", "томат", "сир"],
+ "Готуємо тісто, додаємо інгредієнти та запікаємо", 30)
 
-def create_payment():
+recipe2 =Recipe("Салат",
+ ["томат", "огірок", "зелень", "олія"],
+ "Нарізаємо овочі, додаємо зелень та поливаємо олією", 10)
 
-    #===================== список (!!!list!!!) доступних опцій ===========
-    options = ["CreditCardPayment", "PayPalPayment", "CryptoPayment"]
+recipe3 = Recipe("Суп",
+ ["вода", "картопля", "морква", "м'ясо"],
+ "Варимо всі інгредієнти до готовності",45)
 
-    try:
-        sum_money = float(input("Введіть суму платежу: "))
-    except:
-        print("Неправильна сума платежу")
-        return None
+recipe_list = [recipe1, recipe2, recipe3]
+# тестуємо
+# for _ in recipe_list:
+#     _.display_info()
 
-    currency = input("Введіть валюту платежу: ")
+# Виведіть назви тих рецептів, які містять інгредієнт томат
+print("Інгредієнт 'томат' є в таких рецептах:")
+for item in recipe_list:
+    if "томат" in item:
+        print(item)
 
-    print("Вкажіть тип рахунку.")
+# Виведіть повну інформацію рецепта з найменшим часом приготування
+print("\nРецепт з найменшим часом приготування:")
+min_recipe = min(recipe_list)
+min_recipe.display_info()
 
-    #===================== шаблон імхо буде ==============================
-    print("Виберіть, будь ласка, один з варіантів:")
-    for i, option in enumerate(options, start=1):
-        print(f"\t{option} (або просто введіть {i})")
 
-    choice = input("Ваш вибір: ").strip()
 
-    # Нормалізація: якщо ввів цифру - перетворимо у назву
-    if choice.isdigit():
-        index = int(choice) - 1
-        if 0 <= index < len(options):
-            choice = options[index]
 
-    if choice == "CreditCardPayment":
-        return CreditCardPayment(currency), sum_money
-    elif choice == "PayPalPayment":
-        return PayPalPayment(currency), sum_money
-    elif choice == "CryptoPayment":
-        return CryptoPayment(currency), sum_money
-    else:
-        print("Некоректний вибір!")
-        return None
 
-payments = []
 
-for _ in range(3):
-    result = create_payment()
-    if result:
-        payments.append(result)
 
-print("\n=== Виклик pay() для всіх рахунків ===")
-for payment, amount in payments:
-    payment.pay(amount)
