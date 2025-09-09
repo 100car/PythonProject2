77e71d7eabe6ac05012fd02649afc670db054c4c
100car <serg@tokar.com.ua>
2025-09-09 20:48:51 +0300

    Модуль 11. ООП. Частина 6.

--- main.py
+++ main.py
@@ -1,137 +1,78 @@
 # Завдання 1
-# Створіть клас Pet з атрибутами
-#  name – ім’я тварини
-#  satiety – рівень ситості(від 0 до 100, за замовчуванням 50)
-#  energy – рівень енергії (від 0 до 100, за замовчуванням 50)
-# Методи:
-#  sleep() – збільшує energy до 100
-#  eat(food_amont) – їсть, збільшує satiety на food_amount
-#  play(activity_level) – абстрактний метод
-#  make_sound() – просто pass
-import abc
+# Створіть клас Passenger з атрибутами
+#  name – ім’я
+#  destination – місце, куди прямує
+import typing
 
-# При перевірці ШІ, чат GPT стверджує, що я хотів створити абстрактний клас, то нехай буде
-class Pet(abc.ABC):
-    def __init__(self, name, satiety = 50, energy = 50):
-        if not 0 <= satiety <= 100:
-            raise ValueError("Рівень ситості має бути від 0 до 100.")
-        elif not 0 <= energy <= 100:
-            raise ValueError("Рівень енергії має бути від 0 до 100.")
+class Passenger:
+    def __init__(self, name, destination):
+        self.name = name
+        self.destination = destination
+
+# Завдання 2
+# Створіть клас Transport з атрибутами
+#  speed – швидкість
+# Методи
+#  move(destination, distance) – рухається до місця призначення, виводить інформацію як довго їхали
+
+class Transport:
+    def __init__(self, speed):
+        if speed <= 0:
+            raise ValueError("Швидкість має бути > 0.")
+        self.speed = speed
+
+    def move(self, destination, distance):
+        time_to_destination = distance / self.speed
+        print(f"До місця призначення {destination} доїхали за {time_to_destination:.2f} ГОДИН.")
+
+# Завдання 3
+# Створіть клас Bus з атрибутами
+#  passengers – список пасажирів(об’єкти класу Passenger)
+#  capacity – максимальна можлива кількість пасажирів
+# Методи
+#  board_passenger(passenger) – якщо є місце, додає пасажира
+#  move(destination, distance) – висаджує всіх пасажирів, які хочуть вийти в даному місці
+# (виводить їхню загальну кількість) та викликає батьківський метод move()
+
+class Bus(Transport):
+    def __init__(self, speed: float, capacity: int):
+        super().__init__(speed)
+        self.passengers: typing.List[Passenger] = []
+        self.capacity = capacity
+
+    def board_passenger(self, passenger: Passenger):
+        if len(self.passengers) < self.capacity:
+            self.passengers.append(passenger)
+            print(f"{passenger.name} сів у автобус (їде до {passenger.destination})")
         else:
-            self.name = name
-            self.satiety = satiety
-            self.energy = energy
+            print(f"Автобус переповнений! {passenger.name} не може сісти.")
 
-    def sleep(self):
-        print("Після сну рівень енергії становить 100.")
-        self.energy = 100
+    def move(self, destination, distance):
+        to_exit = [p for p in self.passengers if p.destination == destination]
+        for p in to_exit:
+            self.passengers.remove(p)
 
-    def eat(self, food_amount):
-        change = min(100 - self.satiety, food_amount)
-        self.satiety += change
-        print(f"{self.name} поїв. Рівень ситості: {self.satiety}")
-
-    @abc.abstractmethod
-    def play(self, activity_level):
-        raise  NotImplementedError("Цей метод буде реалізовано в підкласі чи підкласах.")
-
-    def make_sound(self):
-        pass
-
-# Створіть клас Cat
-# Методи:
-#  play(activity_level) – якщо satiety > 60, зменшує energy на 2*acticity_level та satiety на acticity_level
-#  make_sound() – виводить ‘Мяу’
-#  catch_mouse() – якщо energy > 30, ловить мишу. Якщо satiety > 40, то грається з мишею, інакше їсть
+        print(f"У пункті {destination} виходять {len(to_exit)} пасажирів: {[p.name for p in to_exit]}")
+        super().move(destination, distance)
 
 
-class Cat(Pet):
-    def play(self, activity_level):
-        if self.satiety > 60:
-            self.energy = max (self.energy - 2 * activity_level, 0)
-            self.satiety = max (self.satiety - activity_level, 0)
-            # print(f"{self.satiety =}, {self.energy =}")
+p1 = Passenger("Іван", "Київ")
+p2 = Passenger("Оля", "Львів")
+p3 = Passenger("Петро", "Київ")
+p4 = Passenger("Марія", "Одеса")
 
-    def make_sound(self):
-        print("Мяу")
+bus = Bus(speed=120, capacity=3)
 
-    def catch_mouse(self):
-        if self.energy <= 30:
-            print(f"{self.name} не зміг спіймати мишку, його ({self.energy=}.)")
-        else:
-            print(f"{self.name} спіймав мишку ", end="")
-            if self.satiety > 40:
-                print("і грається з нею.")
-            else:
-                print("і їсть бідного гризуна.")
+bus.board_passenger(p1)
+bus.board_passenger(p2)
+bus.board_passenger(p3)
+bus.board_passenger(p4)  # переповнено
 
+print("\n--- Поїздка 1 ---")
+bus.move("Київ", 480)  # 480 км
 
-# Тестування 1
-# tuzik = Pet("Тузик", 50, 100)
-# julbars = Pet("Джульбарс", 100, 100)
-# sirko = Pet("Сірко")
-# murchik = Cat("Мурчик", energy=70, satiety=80)
-# print(murchik.satiety, murchik.energy)
-# murchik.play(70)
-# print(murchik.satiety, murchik.energy)
+print("\n--- Поїздка 2 ---")
+bus.move("Львів", 300)
 
-# Тестування 2
-# murchik = Cat("Мурчик", energy=70, satiety=80)
-# murchik.make_sound()
-# murchik.play(30)
-# murchik.catch_mouse()
-# murchik.sleep()
-# murchik.catch_mouse()
-
-# Створіть клас Dog
-# Методи:
-#  play(activity_level) – якщо satiety > 15, зменшує energy на acticity_level//2 та satiety на acticity_level // 2
-#  make_sound() – виводить ‘Гав’
-#  fetch_ball() – ловить м’яча якщо satiety>10, зменшує energy на 5
-
-class Dog(Pet):
-    def play(self, activity_level):
-        if self.satiety > 15:
-            self.energy = max (self.energy - (activity_level // 2), 0)
-            self.satiety = max (self.satiety - (activity_level // 2), 0)
-            print(f"{self.satiety =}, {self.energy =}")
-        else:
-            print(f"{self.satiety =}, {self.name} ненаїжений, щоб гратися")
-
-    def make_sound(self):
-        print("Гав!!!")
-
-    def fetch_ball(self):
-        if self.satiety > 10:
-            self.energy = max(self.energy - 5, 0)
-            print(f"{self.name} зловив м’яча, його енергія стала {self.energy}")
-        else:
-            print(f"{self.satiety =}, {self.name} ненаїжений, щоб ловити м’яча")
-
-
-tuzik = Dog("Тузик", 50, 100)
-
-# Тестування 3
-print(tuzik.satiety, tuzik.energy)
-tuzik.play(10)
-tuzik.play(40)
-tuzik.play(20)
-tuzik.play(70)
-tuzik.make_sound()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-tuzik.fetch_ball()
-
-
-
+print("\n--- Поїздка 3 ---")
+bus.move("Одеса", 500)
